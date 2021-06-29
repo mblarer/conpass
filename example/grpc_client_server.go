9a20ffda20ecdd5d91404bb1122ecaa4aab569c8
Mathias Blarer <mblarer@protonmail.ch>
2021-06-29 16:56:14 +0200

    Improve gRPC definition and implement segment negotiation skeleton

--- example/grpc_client_server.go
+++ example/grpc_client_server.go
@@ -8,8 +8,8 @@ import (
 	"net"
 	"time"
 
-	"google.golang.org/grpc"
 	pb "github.com/mblarer/scion-ipn/proto/negotiation"
+	grpc "google.golang.org/grpc"
 )
 
 const address = "localhost:1234"
@@ -28,13 +28,34 @@ func main() {
 	}
 }
 
+func runClient() error {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return errors.New(fmt.Sprintf("did not connect: %v", err))
+	}
+	defer conn.Close()
+	c := pb.NewNegotiationServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.NegotiateSegments(ctx, &pb.NegotiationRequest{
+		SegmentSets: generateSegmentSets(),
+	})
+	if err != nil {
+		return errors.New(fmt.Sprintf("could not greet: %v", err))
+	}
+	log.Printf("reply: %s", r.GetSegmentSets())
+	return nil
+}
+
 type server struct {
 	pb.UnimplementedNegotiationServiceServer
 }
 
 func (s *server) NegotiateSegments(cotx context.Context, in *pb.NegotiationRequest) (*pb.NegotiationReply, error) {
-	log.Printf("request: %v", in.GetName())
-	return &pb.NegotiationReply{Message: "fine, send me your segments"}, nil
+	log.Printf("request: %v", in.GetSegmentSets())
+	return &pb.NegotiationReply{
+		SegmentSets: filterSegmentSets(in.GetSegmentSets()),
+	}, nil
 }
 
 func runServer() error {
@@ -51,20 +72,129 @@ func runServer() error {
 	return nil
 }
 
-func runClient() error {
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		return errors.New(fmt.Sprintf("did not connect: %v", err))
+func filterSegmentSets(css []*pb.ClientSegmentSet) []*pb.ServerSegmentSet {
+	sss := make([]*pb.ServerSegmentSet, len(css))
+	for i := 0; i < len(css); i++ {
+		clientSegs := css[i].GetSegments()
+		serverSegs := make([]*pb.ServerSegment, len(clientSegs))
+		for j := 0; j < len(clientSegs); j++ {
+			serverSegs[j] = &pb.ServerSegment{
+				SegmentId:   clientSegs[j].GetSegmentId(),
+				Subsegments: clientSegs[j].GetSubsegments(),
+			}
+		}
+		sss[i] = &pb.ServerSegmentSet{
+			SetId:              css[i].GetSetId(),
+			CompatibleNextSets: css[i].GetCompatibleNextSets(),
+			Segments:           serverSegs,
+		}
 	}
-	defer conn.Close()
-	c := pb.NewNegotiationServiceClient(conn)
-	name := "let's negotiate path segments"
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.NegotiateSegments(ctx, &pb.NegotiationRequest{Name:name})
-	if err != nil {
-		return errors.New(fmt.Sprintf("could not greet: %v", err))
+	return sss
+}
+
+func generateSegmentSets() []*pb.ClientSegmentSet {
+	return []*pb.ClientSegmentSet{
+		// UP: 1#2 --> 2#1,2 --> 3#1
+		&pb.ClientSegmentSet{
+			SetId:              1,
+			CompatibleNextSets: []uint32{2, 3},
+			Segments: []*pb.ClientSegment{
+				&pb.ClientSegment{
+					SegmentId: 1,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 1, Id: 2},
+								Ingress: &pb.Interface{IsdAs: 2, Id: 1},
+							},
+						},
+					},
+				},
+				&pb.ClientSegment{
+					SegmentId: 2,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type:      pb.Subsegment_REFERENCE,
+							Reference: 1,
+						},
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 2, Id: 2},
+								Ingress: &pb.Interface{IsdAs: 3, Id: 1},
+							},
+						},
+					},
+				},
+			},
+		},
+		// CORE: 3#2 --> 4#1
+		// PEER: 2#3 --> 5#3
+		&pb.ClientSegmentSet{
+			SetId:              2,
+			CompatibleNextSets: []uint32{3},
+			Segments: []*pb.ClientSegment{
+				&pb.ClientSegment{
+					SegmentId: 3,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 3, Id: 2},
+								Ingress: &pb.Interface{IsdAs: 4, Id: 1},
+							},
+						},
+					},
+				},
+				&pb.ClientSegment{
+					SegmentId: 4,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 2, Id: 3},
+								Ingress: &pb.Interface{IsdAs: 5, Id: 3},
+							},
+						},
+					},
+				},
+			},
+		},
+		// DOWN: 4#2 --> 5#1,2 --> 6#1
+		&pb.ClientSegmentSet{
+			SetId:              3,
+			CompatibleNextSets: []uint32{},
+			Segments: []*pb.ClientSegment{
+				&pb.ClientSegment{
+					SegmentId: 5,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 5, Id: 2},
+								Ingress: &pb.Interface{IsdAs: 6, Id: 1},
+							},
+						},
+					},
+				},
+				&pb.ClientSegment{
+					SegmentId: 6,
+					Subsegments: []*pb.Subsegment{
+						&pb.Subsegment{
+							Type: pb.Subsegment_LINK,
+							Link: &pb.Link{
+								Egress:  &pb.Interface{IsdAs: 4, Id: 2},
+								Ingress: &pb.Interface{IsdAs: 5, Id: 1},
+							},
+						},
+						&pb.Subsegment{
+							Type:      pb.Subsegment_REFERENCE,
+							Reference: 5,
+						},
+					},
+				},
+			},
+		},
 	}
-	log.Printf("reply: %s", r.GetMessage())
-	return nil
 }
