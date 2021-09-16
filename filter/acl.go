0afa3d384443daedc00078ef15bb4bb0147c8be3
Mathias Blarer <mblarer@protonmail.ch>
2021-09-16 16:27:16 +0200

    Refactor ACL filter implementation

--- filter/acl.go
+++ filter/acl.go
@@ -7,22 +7,23 @@ import (
 	"github.com/scionproto/scion/go/lib/snet"
 )
 
-// ACLFilter implements the segment.Filter interface.
-type ACLFilter struct {
-	ACL pathpol.ACL
+// FromACL returns a segment.Filter that filters path segments according to a
+// pathpol.ACL policy.
+func FromACL(acl pathpol.ACL) segment.Filter {
+	return aclFilter{acl: acl}
 }
 
-func (af ACLFilter) Filter(segset segment.SegmentSet) segment.SegmentSet {
+type aclFilter struct {
+	acl pathpol.ACL
+}
+
+func (af aclFilter) Filter(segset segment.SegmentSet) segment.SegmentSet {
 	return FromPredicate(func(segment segment.Segment) bool {
 		// This implementation is not optimal. If the segment is a segment
 		// composition, then every subsegment should be evaluated only once.
 		path := path.InterfacePath{segment.PathInterfaces()}
-		result := af.ACL.Eval([]snet.Path{path})
+		result := af.acl.Eval([]snet.Path{path})
 		accept := len(result) == 1
 		return accept
 	}).Filter(segset)
-}
-
-func FromACL(acl pathpol.ACL) segment.Filter {
-	return ACLFilter{ACL: acl}
 }
